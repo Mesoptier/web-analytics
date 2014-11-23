a3b730acd2516224a1199062de5191e56cbd5bfb
Michael Yeh <micyeh@live.nl>
2014-11-23 14:24:06 +0100

    spacing and stuff

--- hw1/agent01.cpp
+++ hw1/agent01.cpp
@@ -2,18 +2,18 @@
 #include <sys/time.h>
 
 agent01::agent01(int numberOfKeywords_, double monthBudget_){
-    srand(time(NULL));
-    numberOfKeywords=numberOfKeywords_;
-    monthBudget=monthBudget_;
-    budgetRemaining=monthBudget;
+	srand(time(NULL));
+	numberOfKeywords = numberOfKeywords_;
+	monthBudget = monthBudget_;
+	budgetRemaining = monthBudget;
 
-    // Initialize our variables
-    prevMonth = 0;
+	// Initialize our variables
+	prevMonth = 0;
 
-    impressions.resize(7);
-    for (int i = 0; i < 7; i++) {
-        impressions[i].resize(numberOfKeywords);
-    }
+	impressions.resize(7);
+	for (int i = 0; i < 7; i++) {
+		impressions[i].resize(numberOfKeywords);
+	}
 }
 
 agent01::~agent01(){
@@ -29,68 +29,73 @@ agent01::~agent01(){
 }
 
 void agent01::receiveInfo(int month, int day, int *imps_, int* clicks_, double* cost_, double* pos_){
-    int clicksPerKeyword[numberOfKeywords];
-    int impressionsPerKeyword[numberOfKeywords];
-    double costPerKeyword[numberOfKeywords];
-    double positionPerKeyword[numberOfKeywords];
+	int clicksPerKeyword[numberOfKeywords];
+	int impressionsPerKeyword[numberOfKeywords];
+	double costPerKeyword[numberOfKeywords];
+	double positionPerKeyword[numberOfKeywords];
 
-    double totalCost=0.0;
-    for(int i=0;i<numberOfKeywords;i++){
-        impressionsPerKeyword[i]=imps_[i];
-        clicksPerKeyword[i]=clicks_[i];
-        costPerKeyword[i]=cost_[i];
-        positionPerKeyword[i]=pos_[i];
-        totalCost+=cost_[i];
-    }
+	double totalCost = 0.0;
+	for (int i = 0; i < numberOfKeywords; i++){
+		impressionsPerKeyword[i] = imps_[i];
+		clicksPerKeyword[i] = clicks_[i];
+		costPerKeyword[i] = cost_[i];
+		positionPerKeyword[i] = pos_[i];
+		totalCost += cost_[i];
+	}
 
-    //////////////////////////////////////////////////
-    ///  Below here, make sure you store the info elsewhere and/or make your decisions here.
-    ///  Please ensure that when the month switches you get 1000 euro's extra budget
-    //////////////////////////////////////////////////
+	//////////////////////////////////////////////////
+	///  Below here, make sure you store the info elsewhere and/or make your decisions here.
+	///  Please ensure that when the month switches you get 1000 euro's extra budget
+	//////////////////////////////////////////////////
 
-    // If yesterday's month != today's month, increase the budget
-    if (prevMonth != month) {
-        prevMonth = month;
-        budgetRemaining += 1000;
-    }
+	// If yesterday's month != today's month, increase the budget
+	if (prevMonth != month) {
+		prevMonth = month;
+		budgetRemaining += 1000;
+	}
 
 
-    //////////////////////////////////////////////////
-    //////////////////////////////////////////////////
+	//////////////////////////////////////////////////
+	//////////////////////////////////////////////////
 }
 
 double* agent01::getBidsForAllKeywords(){
-    double* allBids = new double[numberOfKeywords];     //Don't worry about garbage collection, is handled elsewhere!!!
-    for(int i=0;i<numberOfKeywords;i++)
-        allBids[i]=getRandBetween0and1()*0.10;
+	double* allBids = new double[numberOfKeywords];     //Don't worry about garbage collection, is handled elsewhere!!!
+	for (int i = 0; i < numberOfKeywords; i++)
+		allBids[i] = getRandBetween0and1()*0.10;
 
-    //////////////////////////////////////////////////
-    ///  Below here, make code to set the correct bids for the simulation
-    //////////////////////////////////////////////////
-
-
+	//////////////////////////////////////////////////
+	///  Below here, make code to set the correct bids for the simulation
+	//////////////////////////////////////////////////
 
 
-    //////////////////////////////////////////////////
-    //////////////////////////////////////////////////
 
-    return allBids;
+
+	//////////////////////////////////////////////////
+	//////////////////////////////////////////////////
+
+	return allBids;
 }
 
 double* agent01::getMaxBudgetForAllKeywords(){
-    double* allBudget = new double[numberOfKeywords];   //Garbage collection handled elsewhere
-    for(int i=0;i<numberOfKeywords;i++)
-        allBudget[i]=1000.0/(numberOfKeywords*30);
+	double* allBudget = new double[numberOfKeywords];   //Garbage collection handled elsewhere
+	for (int i = 0; i < numberOfKeywords; i++)
+		allBudget[i] = 1000.0 / (numberOfKeywords * 30);
 
-    //////////////////////////////////////////////////
-    ///  Below here, make code to set the correct budgets for the simulation
-    //////////////////////////////////////////////////
+	//////////////////////////////////////////////////
+	///  Below here, make code to set the correct budgets for the simulation
+	//////////////////////////////////////////////////
+	if (prevMonth == 0){
+
+	}
+	else {
+
+	}
 
 
 
+	//////////////////////////////////////////////////
+	//////////////////////////////////////////////////
 
-    //////////////////////////////////////////////////
-    //////////////////////////////////////////////////
-
-    return allBudget;
+	return allBudget;
 }
--- hw1/agent01.h
+++ hw1/agent01.h
@@ -5,20 +5,20 @@
 
 class agent01{
 public:
-    agent01(int numberOfKeywords_,double monthBudget_);
-    ~agent01();
-    void receiveInfo(int month, int day, int* imps_, int* clicks_, double* cost_, double* pos_);
-    double* getBidsForAllKeywords();
-    double* getMaxBudgetForAllKeywords();
+	agent01(int numberOfKeywords_, double monthBudget_);
+	~agent01();
+	void receiveInfo(int month, int day, int* imps_, int* clicks_, double* cost_, double* pos_);
+	double* getBidsForAllKeywords();
+	double* getMaxBudgetForAllKeywords();
 
 private:
-    int numberOfKeywords;
-    double getRandBetween0and1(){return ((double)rand())/RAND_MAX;}
-    double monthBudget;
-    double budgetRemaining;
+	int numberOfKeywords;
+	double getRandBetween0and1(){ return ((double)rand()) / RAND_MAX; }
+	double monthBudget;
+	double budgetRemaining;
 
-    int prevMonth;
-    vector< vector< vector<int> > > impressions;
+	int prevMonth;
+	vector< vector< vector<int> > > impressions;
 };
 
 #endif // AGENT01_H
