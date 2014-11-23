4638b5ee542b945445c7ed354a92afb27b3c2c32
stanrogo <stanrogo@outlook.com>
2014-11-23 14:29:05 +0100

    resolved my merge conflicts

--- hw1/agent01.h
+++ hw1/agent01.h
@@ -5,19 +5,11 @@
 
 class agent01{
 public:
-<<<<<<< Updated upstream
 	agent01(int numberOfKeywords_, double monthBudget_);
 	~agent01();
 	void receiveInfo(int month, int day, int* imps_, int* clicks_, double* cost_, double* pos_);
 	double* getBidsForAllKeywords();
 	double* getMaxBudgetForAllKeywords();
-=======
-    agent01(int numberOfKeywords_,double monthBudget_);
-    ~agent01();
-    void receiveInfo(int month, int day, int* imps_, int* clicks_, double* cost_, double* pos_);
-    double* getBidsForAllKeywords();
-    double* getMaxBudgetForAllKeywords();
->>>>>>> Stashed changes
 
 private:
 	int numberOfKeywords;
@@ -25,13 +17,9 @@ private:
 	double monthBudget;
 	double budgetRemaining;
 
-<<<<<<< Updated upstream
 	int prevMonth;
 	vector< vector< vector<int> > > impressions;
-=======
-    int prevMonth;
     int prevDayOfMonth;
->>>>>>> Stashed changes
 };
 
 #endif // AGENT01_H
